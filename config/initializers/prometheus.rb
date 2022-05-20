edd126dfec74c0ae0c59457eb6093cc145f6d389
Gavin Morrice <gavin@gavinmorrice.com>
2022-05-20 17:10:58 +0100

    Refactor bucket definition for easier legibility

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -7,5 +7,5 @@ Rails.application.middleware.use Prometheus::Client::Rack::Exporter
 
 Promenade.histogram :rails_app_car_speed_mph do
   doc "Records the speed of each car in MPH"
-  buckets ((1..70).select { |int| int % 10 == 0 })
+  buckets [10, 20, 30, 40, 50, 60, 70]
 end
